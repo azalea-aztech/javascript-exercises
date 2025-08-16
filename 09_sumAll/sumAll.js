0b5774c8ee6efd6d85cea05fd1fd082195ccf025
Azalea <azalea.aztech@gmail.com>
2025-08-16 17:57:09 +0300

    09 DONE

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y)) {
+        return "ERROR";
+    }
+    var sum = 0;
+    for (let i = Math.min(x, y); i <= Math.max(x, y); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
