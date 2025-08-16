22a3802195c3e67dd79124e5862bbab4bf913222
Azalea <azalea.aztech@gmail.com>
2025-08-16 16:56:31 +0300

    07 DONE

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var reversed = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversed += string[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
