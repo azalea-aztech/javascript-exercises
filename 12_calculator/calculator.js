9f827bb0ab185f7a2874c28222f179d78ad2ea77
Azalea <azalea.aztech@gmail.com>
2025-08-17 20:17:00 +0300

    12 DONE

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = a;
+	for (let i = 1; i < b; i++){
+    result = result * a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = a;
+  if (a == 0) return 1;
+  while(a > 1) {
+    a = a - 1;
+    result = result * a;
+  }
+  return result;
 };
 
 // Do not edit below this line
