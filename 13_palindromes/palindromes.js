c5dee7f68591881eecc3327ad3f2116850660fff
Azalea <azalea.aztech@gmail.com>
2025-08-18 23:54:31 +0300

    13 DONE

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleanString = "";
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === " " || string[i] === "," || string[i] === "." || string[i] === "!") {
+            continue;
+        } else {
+            cleanString += string[i].toLowerCase();
+        }
+    }
+    let backwardsString = "";
+    for (let i = cleanString.length - 1; i >= 0; i--) {
+        backwardsString += cleanString[i];
+    }
 
+    if (cleanString === backwardsString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
