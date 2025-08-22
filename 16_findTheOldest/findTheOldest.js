a980d5a64494fd6abf5be3abf7177c5f6b29516e
Azalea <azalea.aztech@gmail.com>
2025-08-22 17:05:43 +0300

    16 DONE

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const returnAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+    const oldestAge = returnAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = returnAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
