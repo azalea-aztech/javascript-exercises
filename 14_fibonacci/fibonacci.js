cb7dbd84725c4831242999af4e59e67c03192ea0
Azalea <azalea.aztech@gmail.com>
2025-08-22 16:29:07 +0300

    14 DONE

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS";
+    if(num == 1) return 1;
+    let prev1 = 0;
+    let prev2 = 1;
+    let current = 0;
+    for (let i = 2; i <= num; i++) {
+        current = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = current;
+    }
+    return current;
 };
 
 // Do not edit below this line
